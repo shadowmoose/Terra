15bd6846b0da93d7678ae25191c98866680ec93c
Mike <theshadowmoose@gmail.com>
2020-07-23 19:12:31 -0700

    Add unstable builds (#13)
    * Enable unstable build directory.

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 
 2D Tabletop sprite-based world editor. WIP.
 
-https://shadowmoose.github.io/Terra/
+
+This project is currently under heavy development. 
+Until it is officially deployed to a "real" URL, you can find resources at the below locations:
+
+__Latest Master Build:__ https://shadowmoose.github.io/Terra/
+
+__Latest Unstable Build:__ https://shadowmoose.github.io/Terra/unstable/
+
+__Project Status:__ https://github.com/shadowmoose/Terra/projects/1
+
+__More Info:__ https://github.com/shadowmoose/Terra/wiki
--- web-ui/src/ui-components/boardSaveButton.tsx
+++ web-ui/src/ui-components/boardSaveButton.tsx
@@ -3,11 +3,14 @@ import {Fab, Tooltip} from "@material-ui/core";
 import SaveIcon from '@material-ui/icons/Save';
 import React from "react";
 import GameController from "../game/controllers/game";
+import {netMode, NetworkMode} from "../game/net/peerconnection";
 
 
 export const BoardSaveButton = observer( (props: {controller: GameController}) => {
     const shouldSave = props.controller.campaign?.loadedBoard &&
         (props.controller.entities.isDirty || props.controller.terrain.isBoardDirty);
+
+    if (netMode.get() !== NetworkMode.HOST) return null;
 
     return <Tooltip
         title={shouldSave ? "Save Board" : "No changes to save."}
