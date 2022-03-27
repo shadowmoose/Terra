a61bab0e8f74f119a613878637c7367287b648c5
ShadowMoose <theshadowmoose@gmail.com>
2022-03-27 01:44:15 -0700

    Add missing prop.

--- src/ui-components/spritepicker.tsx
+++ src/ui-components/spritepicker.tsx
@@ -48,7 +48,7 @@ export default function SpritePicker (
         return () => {
             clearTimeout(timeout);
         }
-    }, [searchTerm, animated]);
+    }, [searchTerm, animated, props.forEntity]);
 
 
     return <div style={{
